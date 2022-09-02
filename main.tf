c26e07785228abee0235a7d69ec04a82f0a04706
Pratik Kumar <prakumar125@publicisgroupe.net>
2022-09-02 16:06:20 +0530

    asad

--- main.tf
+++ main.tf
@@ -15,4 +15,13 @@ terraform {
       name = "pk-actions-demo1"
     }
   }
+
+  resource "aws_s3_bucket" "b3443" {
+  bucket = "my-tf-s3-bucket4"
+
+  tags = {
+    Name        = "My bucket"
+    Environment = "Dev"
+  }
+}
 }
